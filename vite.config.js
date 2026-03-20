a2e45623b77acbd2dbbaf4bffb5e6e67ee0b308c
Jdig Oussama <jdigoussama@gmail.com>
2026-03-20 21:35:10 +0000

    edit

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { traeBadgePlugin } from 'vite-plugin-trae-solo-badge';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
@@ -24,15 +23,6 @@ export default defineConfig({
           'react-dev-locator',
         ],
       },
-    }),
-    traeBadgePlugin({
-      variant: 'dark',
-      position: 'bottom-right',
-      prodOnly: true,
-      clickable: true,
-      clickUrl: 'https://www.trae.ai/solo?showJoin=1',
-      autoTheme: true,
-      autoThemeTarget: '#root'
     }),
   ],
 })
